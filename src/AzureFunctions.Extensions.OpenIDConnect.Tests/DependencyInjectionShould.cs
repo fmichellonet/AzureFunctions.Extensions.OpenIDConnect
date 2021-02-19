40699beac6b28db1bf24669c0830c586330b4b19
Fabrice Michellonet <fabrice.michellonet@gmail.com>
2021-02-19 22:44:38 +0100

    Fix bad dependency injection Added dependency injection test

---
+++ src/AzureFunctions.Extensions.OpenIDConnect.Tests/DependencyInjectionShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AzureFunctions.Extensions.OpenIDConnect.Configuration;
+using FluentAssertions;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace AzureFunctions.Extensions.OpenIDConnect.Tests
+{
+    [TestFixture]
+    public class DependencyInjectionShould
+    {
+        [Test]
+        public void Be_Resolvable()
+        {
+            var collection = new ServiceCollection();
+            collection.AddOpenIDConnect(builder =>
+            {
+                builder.SetIssuerBaseUrlConfiguration("https://issuer.com/");
+                builder.SetTokenValidation("issuer", "audience");
+                builder.SetTypeCrawler(() => new Type[] { });
+            });
+
+            var provider = collection.BuildServiceProvider();
+
+            provider.GetService<IAuthenticationService>().Should().NotBe(null);
+            provider.GetService<IRouteGuardian>().Should().NotBe(null);
+            provider.GetService<IFunctionFilter>().Should().NotBe(null);
+        }
+    }
+}
--- src/AzureFunctions.Extensions.OpenIDConnect/Configuration/ServicesConfigurationExtensions.cs
+++ src/AzureFunctions.Extensions.OpenIDConnect/Configuration/ServicesConfigurationExtensions.cs
@@ -44,7 +44,7 @@
             services.AddSingleton<IJwtSecurityTokenHandlerWrapper, JwtSecurityTokenHandlerWrapper>();
             services.AddSingleton<IOpenIdConnectConfigurationManager, OpenIdConnectConfigurationManager>();
             services.AddSingleton<IAuthenticationService, AuthenticationService>();
-            services.AddSingleton<RouteGuardian>();
+            services.AddSingleton<IRouteGuardian, RouteGuardian>();
             services.AddSingleton<IFunctionFilter, AuthorizeFilter>();
         }
     }
